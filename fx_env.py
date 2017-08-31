310764334fdf5e875a71123303dbecb1c1b86d4b
hide-tono <information@tono-n-chi.com>
2017-09-01 07:18:49 +0900

    クローズ処理、ロット数追加

--- fx_env.py
+++ fx_env.py
@@ -19,8 +19,6 @@ class FxEnv(gym.Env):
         self.BUY = 1
         self.SELL = 2
         self.CLOSE = 3
-        # 初期のバランス
-        self.initial_balance = 10000
         # CSVファイルのパス
         self.csv_file_path = csv_path
         # スプレッド
@@ -31,9 +29,16 @@ class FxEnv(gym.Env):
         self.take_profit_pips = 30
         # 損切りpips
         self.stop_loss_pips = 15
+        # ロット数
+        self.lots = 0.1
 
     def _reset(self):
-        self.balance = self.initial_balance
+        # 総獲得pips
+        self.total_pips = 0
+        # 総獲得pips(買い)
+        self.total_pips_buy = 0
+        # 総獲得pips(売り)
+        self.total_pips_sell = 0
         # CSVを読み込む
         self.data = pandas.read_csv(self.csv_file_path,
                                     names=['date', 'time', 'o', 'h', 'l', 'c', 'v'],
@@ -54,21 +59,26 @@ class FxEnv(gym.Env):
             pass
         elif action == self.BUY:
             ticket = Ticket(self.BUY, ask, ask + self.take_profit_pips * self.point,
-                            ask - self.stop_loss_pips * self.point)
+                            ask - self.stop_loss_pips * self.point, self.lots)
             self.tickets.append(ticket)
             pass
         elif action == self.SELL:
             ticket = Ticket(self.SELL, bid, bid - self.take_profit_pips * self.point,
-                            bid + self.stop_loss_pips * self.point)
+                            bid + self.stop_loss_pips * self.point, self.lots)
             self.tickets.append(ticket)
             pass
         elif action == self.CLOSE:
             if len(self.tickets) > 0:
                 for i in range(len(self.tickets)):
-                    if self.tickets[i].type == self.BUY:
-                        # TODO
-                        pass
-                        # profit =
+                    ticket = self.tickets[i]
+                    if ticket.order_type == self.BUY:
+                        profit = (bid - ticket.open_price) * ticket.lots
+                        self.total_pips += profit
+                        self.total_pips_buy += profit
+                    elif ticket.order_type == self.SELL:
+                        profit = (ticket.open_price - ask) * ticket.lots
+                        self.total_pips += profit
+                        self.total_pips_sell += profit
 
         # インデックスをインクリメント
         self.read_index += 1
@@ -81,12 +91,14 @@ class FxEnv(gym.Env):
 
 
 class Ticket(object):
-    def __init__(self, type, open_price, take_profit, stop_loss):
+    def __init__(self, order_type, open_price, take_profit, stop_loss, lots):
         # タイプ
-        self.type = type
+        self.order_type = order_type
         # 約定価格
         self.open_price = open_price
         # 利食い価格
         self.take_profit = take_profit
         # 損切り価格
         self.stop_loss = stop_loss
+        # ロット
+        self.lots = lots
